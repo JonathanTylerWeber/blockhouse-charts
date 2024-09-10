364fe479004f51204e5dec2def32d33b3042e0ee
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-09-10 22:24:53 +0800

    routing and styles

--- frontend/chart-project/app/bar/page.tsx
+++ frontend/chart-project/app/bar/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import BarChartComp from '@/components/BarChartComp';
 import { useSelector } from 'react-redux';
 import { RootState } from "../redux/store";
@@ -7,6 +9,8 @@ export default function Page() {
   const barData = useSelector((state: RootState) => state.bar.data);
 
   return (
-    <BarChartComp barData={barData} />
+    <div className="flex justify-center items-center m-10">
+      <BarChartComp barData={barData} width={800} height={600} />
+    </div>
   )
 }
